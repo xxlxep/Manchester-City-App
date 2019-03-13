cdc6c5a6c5fc0c738a901e0abd0f7ef63b71a4e6
xxlxep <xxlxep@gmail.com>
2019-03-13 09:22:03 +0300

    Animate Stripes in Featured

---
+++ src/Components/Home/Featured/Stripes.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { easePolyOut } from 'd3-ease';
+import Animate from 'react-move/Animate';
+
+export default class Stripes extends Component {
+
+state = {
+    stripes:[
+        {
+            background: '#98c5e9',
+            left: 120,
+            rotate: 25,
+            top: -260,
+            delay: 0
+        },
+        {
+            background: '#ffffff',
+            left: 360,
+            rotate: 25,
+            top: -397,
+            delay: 200
+        },
+        {
+            background: '#98c5e9',
+            left: 600,
+            rotate: 25,
+            top: -490,
+            delay: 400
+        }
+    ] 
+} 
+
+showStripes = () => (
+    this.state.stripes.map((stripe,id) => (
+        <Animate
+            key={id}
+            show={true}
+            start={{
+                background: '#ffffff',
+                opacity: 0,
+                left: 0,
+                rotate: 0,
+                top: 0
+            }}
+            enter={{
+                background: stripe.background,
+                opacity: [1],
+                left: [stripe.left],
+                rotate: [stripe.rotate],
+                top: [stripe.top],
+                timing: {delay: stripe.delay, duration: 200, ease: easePolyOut}
+            }}
+        >
+            {({opacity, left, rotate, top, background}) => {
+                return(
+                        <div
+                            className="stripe"
+                            style={{
+                                background,
+                                opacity,
+                                transform: `rotate(${rotate}deg) translate(${left}px, ${top}px)`
+                            }}
+                        >
+                        {console.log(background)}
+                        </div>
+                );
+            }}
+        </Animate>
+    ))
+)
+
+  render() {
+    return (
+      <div className="featured_stripes">
+            {this.showStripes()}
+      </div>
+    )
+  }
+}
