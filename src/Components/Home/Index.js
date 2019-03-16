a6643d319d8a83e2be27eb66af899b5968303f5a
xxlxep <xxlxep@gmail.com>
2019-03-16 09:28:58 +0300

    Win a Jersey

--- src/Components/Home/Index.js
+++ src/Components/Home/Index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Featured from './Featured/Index';
 import Matches from './Matches/index';
 import MeetPlayers from './meetPlayers';
+import Promotion from './Promotion/index';
 
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
             <Featured/>
             <Matches />
             <MeetPlayers />
+            <Promotion />
         </div>
     );
 };
