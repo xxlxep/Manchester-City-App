55ee074eabadf78a19bc6c4c2f82bab0b602ecb9
xxlxep <xxlxep@gmail.com>
2019-04-26 13:56:28 +0300

    Public routes added

--- src/Routes.js
+++ src/Routes.js
@@ -6,14 +6,16 @@ import SignIn from './Components/signin/index';
 
 import Dashboard from './Components/admin/Dashboard';
 import PrivateRoute from './Components/authRoutes/privateRoutes';
+import PublicRoute from './Components/authRoutes/publicRoutes';
+
 
 const Routes = (props) => {
   return (
     <Layout>
       <Switch>
         <PrivateRoute {...props} path="/dashboard" exact component={Dashboard} />
-        <Route exact component={SignIn} path="/sign_in" />
-        <Route exact component={Home} path="/" />
+        <PublicRoute {...props} restricted={true} path="/sign_in" exact component={SignIn} />
+        <PublicRoute {...props} restricted={false} path="/" exact component={Home} />
       </Switch>
     </Layout>  
   )
