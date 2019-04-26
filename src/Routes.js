5d2525c98a0c63eeb635d15ed0fb2160fbe94d92
xxlxep <xxlxep@gmail.com>
2019-04-26 17:49:52 +0300

    matches endpoint

--- src/Routes.js
+++ src/Routes.js
@@ -7,12 +7,14 @@ import SignIn from './Components/signin/index';
 import Dashboard from './Components/admin/Dashboard';
 import PrivateRoute from './Components/authRoutes/privateRoutes';
 import PublicRoute from './Components/authRoutes/publicRoutes';
+import AdminMatches from './Components/admin/matches';
 
 
 const Routes = (props) => {
   return (
     <Layout>
       <Switch>
+        <PrivateRoute {...props} path="/admin_matches" exact component={AdminMatches} />
         <PrivateRoute {...props} path="/dashboard" exact component={Dashboard} />
         <PublicRoute {...props} restricted={true} path="/sign_in" exact component={SignIn} />
         <PublicRoute {...props} restricted={false} path="/" exact component={Home} />
