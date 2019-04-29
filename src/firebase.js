57e72b446a063e9a280b0187fe7e326e3317c5a8
xxlxep <xxlxep@gmail.com>
2019-04-29 16:29:20 +0300

    addEditMatch with errors in updateForm

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/database';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyBQZIc1LNc2cr3lRmM-crLhG9zQhx_0vHQ",
-    authDomain: "manchester-city-app-6d8f0.firebaseapp.com",
-    databaseURL: "https://manchester-city-app-6d8f0.firebaseio.com",
-    projectId: "manchester-city-app-6d8f0",
-    storageBucket: "manchester-city-app-6d8f0.appspot.com",
-    messagingSenderId: "477140412402"
-  };
+  apiKey: 'AIzaSyBQZIc1LNc2cr3lRmM-crLhG9zQhx_0vHQ',
+  authDomain: 'manchester-city-app-6d8f0.firebaseapp.com',
+  databaseURL: 'https://manchester-city-app-6d8f0.firebaseio.com',
+  projectId: 'manchester-city-app-6d8f0',
+  storageBucket: 'manchester-city-app-6d8f0.appspot.com',
+  messagingSenderId: '477140412402',
+};
 firebase.initializeApp(config);
 
 const firebaseDB = firebase.database();
@@ -18,11 +18,12 @@ const firebaseDB = firebase.database();
 const firebaseMatches = firebaseDB.ref('matches');
 
 const firebasePromotions = firebaseDB.ref('promotions');
+const firebaseTeams = firebaseDB.ref('teams');
 
-export{
-    firebase,
-    firebaseMatches,
-    firebasePromotions
-}
-
-  
+export {
+  firebase,
+  firebaseMatches,
+  firebasePromotions,
+  firebaseTeams,
+  firebaseDB,
+};
